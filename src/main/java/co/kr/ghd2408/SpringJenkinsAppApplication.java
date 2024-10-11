8dec6875adf56e31879f74eb5f9c97355096afc5
SeoHong23 <ghd2408@naver.com>
2024-10-11 11:30:09 +0900

    first commit

---
+++ src/main/java/co/kr/ghd2408/SpringJenkinsAppApplication.java
@@ -0,0 +1,13 @@
+package co.kr.ghd2408;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/co/kr/ghd2408/controller/MainController.java
@@ -0,0 +1,12 @@
+package co.kr.ghd2408.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "/index";
+    }
+}
